0250e42afd3e7429c3f42042cd0990b1b9bbc6da
Mohan krishna Alavala <alavalamohan@gmail.com>
2020-03-02 11:49:00 -0600

    code

--- fun.js
+++ fun.js
@@ -2,14 +2,10 @@
 function convert(degree) {
   var x;
   if (degree == "C") {
-    x = document.getElementById("c").value * 9.0/ 5.0 + 32.0;
+    x = document.getElementById("c").value * 9.0 / 5.0 + 32.0;
     document.getElementById("f").value = Math.round(x);
-  }
-  /* else {
+  } else {
     x = (document.getElementById("f").value -32) * 5 / 9;
     document.getElementById("c").value = Math.round(x);
   }
-  */
 }
-
-
